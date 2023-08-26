3308a63a5247b04fe6d15eb4258f84ea7cf78818
Wong Lok <wonglok@users.noreply.github.com>
2023-08-26 16:09:59 +0800

    fa

--- public/swan/preload.modern.js
+++
@@ -1,1 +0,0 @@
-async function e({loadGlobals:e}){await e({globals:[{name:"react",needs:!0},{name:"three",needs:!0},{name:"zustand",needs:!0},{name:"@react-three/fiber",needs:!0},{name:"@react-three/drei",needs:!0},{name:"@react-three/postprocessing",needs:!1},{name:"@react-three/xr",needs:!1},{name:"three-stdlib",needs:!1},{name:"agape-sdk",needs:!1}]})}export{e as preload};
