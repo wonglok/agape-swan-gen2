e5ea756c92d86049bfee70a2feff7c6446392179
Wong Lok <wonglok@users.noreply.github.com>
2023-09-03 11:25:15 +0800

    fa

--- pages/blender.jsx
+++ pages/blender.jsx
@@ -1,6 +1,11 @@
 import { Blender } from "src-blender/Blender";
 
-export default Blender;
+export default function Page() {
+  if (process.env.NODE_ENV !== "development") {
+    return <div>Blender Livelink is ready on local development server</div>;
+  }
+  return <Blender></Blender>;
+}
 //
 /*
 
