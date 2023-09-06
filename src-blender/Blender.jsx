459f643ec22426974a381ab06d863abddb0b64ea
Wong Lok <wonglok@users.noreply.github.com>
2023-09-06 09:11:31 +0800

    wow

--- src-blender/Blender.jsx
+++ src-blender/Blender.jsx
@@ -26,13 +26,9 @@ export function Blender() {
     <>
       <div className='flex w-full h-full'>
         <div className='' style={{ width: `270px`, height: '100%' }}>
-          <div className='overflow-x-scroll m-2 bg-blue-100 p-2 hover:bg-blue-200 cursor-pointer relative rounded-lg'>
-            <div className='text-xl font-bold text-center text-gray-600' style={{ fontSize: '14px' }}>
-              Blender Livelink
-            </div>
-            <div className='text-xs text-center text-gray-600' style={{ fontSize: '12px' }}>
-              Click Boxes Below to Copy URL
-            </div>
+          <div className='overflow-x-scroll py-5 m-2 bg-blue-100 p-2 hover:bg-blue-200 cursor-pointer relative rounded-lg'>
+            <div className='text-xl font-bold text-center text-blue-800'>Blender Livelink</div>
+            <div className='text-xs text-center text-gray-500'>Click Boxes Below to Copy URL</div>
           </div>
 
           {files.map((it, idx) => {
