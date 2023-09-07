0e7f3f098c79661fb355f27b004002d8e0d03fc1
Wong Lok <wonglok@users.noreply.github.com>
2023-09-07 15:12:09 +0800

    fafafa

--- src-preview/Preview.jsx
+++ src-preview/Preview.jsx
@@ -48,7 +48,7 @@ function Preview() {
 
         {/* Remote Loading... */}
         {/* For loading production code */}
-        <group position={[3, 0, 0]}>
+        <group position={[0, 0, 0]}>
           <Suspense fallback={null}>
             <SwanRemoteRuntimeGen2
               baseURL={`https://agape-swan-gen2.vercel.app`}
